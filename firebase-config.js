e9e1c15523c434f218028679a57ac0b17057b421
susetiadi2 <susetiadi6@gmail.com>
2025-07-19 08:56:05 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,39 @@
+{
+  "project_info": {
+    "project_number": "313811173554",
+    "project_id": "jurnalapp-3efb7",
+    "storage_bucket": "jurnalapp-3efb7.firebasestorage.app"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:313811173554:android:d3bd816121efc30f5b09ab",
+        "android_client_info": {
+          "package_name": "com.jurnalguru"
+        }
+      },
+      "oauth_client": [
+        {
+          "client_id": "313811173554-4k1os3esbq3i19es5qu5pfpgfe212qso.apps.googleusercontent.com",
+          "client_type": 3
+        }
+      ],
+      "api_key": [
+        {
+          "current_key": "AIzaSyAZo6UpRbi-g7jfbHdjiaKFEo2gJhO5l08"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": [
+            {
+              "client_id": "313811173554-4k1os3esbq3i19es5qu5pfpgfe212qso.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ]
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
